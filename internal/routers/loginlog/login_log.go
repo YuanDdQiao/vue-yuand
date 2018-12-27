2efa16fa185b26902b6a67b53a3c6c296587b9bf
Administrator <admin@example.com>
2018-12-27 15:18:18 +0800

    go代码提交

---
+++ internal/routers/loginlog/login_log.go
@@ -0,0 +1,27 @@
+package loginlog
+
+import (
+	"gocron/internal/models"
+	"gocron/internal/modules/logger"
+	"gocron/internal/modules/utils"
+	"gocron/internal/routers/base"
+	"gopkg.in/macaron.v1"
+)
+
+func Index(ctx *macaron.Context) string {
+	loginLogModel := new(models.LoginLog)
+	params := models.CommonMap{}
+	base.ParsePageAndPageSize(ctx, params)
+	total, err := loginLogModel.Total()
+	loginLogs, err := loginLogModel.List(params)
+	if err != nil {
+		logger.Error(err)
+	}
+
+	jsonResp := utils.JsonResponse{}
+
+	return jsonResp.Success(utils.SuccessContent, map[string]interface{}{
+		"total": total,
+		"data":  loginLogs,
+	})
+}
